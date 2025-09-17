0b77049c6baaef22b69080bca39c7569dfe8cd91
filipe-wesley <filipewesley06@gmail.com>
2025-09-17 16:57:40 -0300

    Add files via upload

---
+++ ex15.py
@@ -0,0 +1,6 @@
+tamanho = float(input("Quantos metros quadrados devem ser pintados? "))
+litro = 1
+rende_litro = tamanho/3
+lata = litro*18
+custo = lata*80
+print("Você terá que comprar: ",lata,"latas e custará: R$",custo,"")
